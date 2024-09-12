db0be37af08bf050c6d21a88ec3811db0667949b
M4ALV3S <marcos_14alves@outlook.com>
2024-09-12 11:07:56 -0300

    Runss

--- setup/settings.py
+++ setup/settings.py
@@ -29,9 +29,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
-
-
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+WSGI_APPLICATION = 'setup.wsgi.app'
 # Application definition
 
 INSTALLED_APPS = [
